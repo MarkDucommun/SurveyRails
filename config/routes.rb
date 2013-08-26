4a0f5af42bc0d61aa2729e73efd36ebfb4a92706
dbc-apprentice <apprentice@devbootcamp.com>
2013-08-26 14:49:37 -0500

    CRUD routes for user, session added

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,12 @@
 SurveyRails::Application.routes.draw do
+  
+  resources :users
+  resource :session, :only => [ :new, :create, :destroy ]
+
+
   # The priority is based upon order of creation:
   # first created -> highest priority.
-
+  
   # Sample of regular route:
   #   match 'products/:id' => 'catalog#view'
   # Keep in mind you can assign values other than :controller and :action
