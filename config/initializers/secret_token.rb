9cef53542737d979cb8dea98547a36ae8d9a2149
dbc-apprentice <apprentice@devbootcamp.com>
2013-08-26 10:55:18 -0500

    rails add skeleton

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SurveyRails::Application.config.secret_token = '326116bd5af194adcd85a2d629d70fb699ea03c3117af6ebb325e9c396d26716bfa342aad3f116d07c6a9fccde9551157e45bc22601fe634572875bf43f89d08'
